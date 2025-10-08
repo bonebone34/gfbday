cfed8e4c46c8744b521a2c025e21dbc73d40923c
bonebone34 <ultorgaming14@gmail.com>
2025-10-08 22:13:38 +0800

    updates

--- happybdaybb/next.config.ts
+++ happybdaybb/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
